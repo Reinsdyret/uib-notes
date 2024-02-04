0316b5aae8d2dfa3a5c013e47a2ff4a0aa0c2d63
Lars Haukland <ves019@uib.no>
2024-02-04 22:30:36 +0100

    Gss

--- inf237/weeks/week3/reconnaissance/src/main.rs
+++ inf237/weeks/week3/reconnaissance/src/main.rs
@@ -1,83 +1,107 @@
+use core::num;
 use std::{cmp, io};
 use std::convert::TryInto;
 use std::process;
 
 fn main() {
 
-    let [n]: [i32;1] = get_i32s().try_into().expect("Expected 1 int");
+    let [n]: [f64;1] = get_f64s().try_into().expect("Expected 1 int");
 
-    if n == 1 {
+    if n == 1.0 {
         println!("0");
         process::exit(0);
     }
 
-    let mut largest: usize = 0;
-    let mut smallest: usize = 0;
-    let mut numbers: Vec<(i32, i32)> = Vec::new();
+    let mut numbers: Vec<(f64, f64)> = Vec::new();
 
-    let mut all0v: bool = true;
     // Get one input first so we can compare largest and smallest
-    let [x, v]: [i32; 2] = get_i32s().try_into().expect("Expected two ints");
+    let [x, v]: [f64; 2] = get_f64s().try_into().expect("Expected two ints");
     numbers.push((x, v));
 
-    for i in 1..n {
-        let [x, v]: [i32; 2] = get_i32s().try_into().expect("Expected two ints");
+    for i in 1..n as i64 {
+        let [x, v]: [f64; 2] = get_f64s().try_into().expect("Expected two ints");
         numbers.push((x, v));
-        if x > numbers[largest].0 {
-            largest = i as usize;
-        } else if x < numbers[smallest].0 {
-            smallest = i as usize;
-        }
-
-        if v != 0{
-            all0v = false
-        }
     }
 
-    numbers.sort();
+    let x = gss(fp, 0.0, 1000.0, 0.0001, f64::sqrt(5.0) + 1.0 / 2.0, &numbers);
 
-    let mut prev_distance = diff(&numbers[largest].0, &numbers[smallest].0);
-
-    if all0v{
-        println!("{}", prev_distance);
-        process::exit(0);
-    }
-
-    while diff(&numbers[largest].0, &numbers[smallest].0) <= prev_distance {
-        for i in 0..numbers.len() {
-            numbers[i].0 += numbers[i].1;
-            if numbers[i].0 > numbers[largest].0 {
-                largest = i;
-            } else if numbers[i].0 < numbers[smallest].0 {
-                smallest = i;
-            }
-        }
-
-        //println!("{:?}, smallest:{}, largest:{}, diff: {}", numbers, numbers[smallest].0, numbers[largest].0, diff(&numbers[largest].0, &numbers[smallest].0));
-
-        if diff(&numbers[largest].0, &numbers[smallest].0) > prev_distance {
-            break;
-        }
-
-
-        prev_distance = diff(&numbers[largest].0, &numbers[smallest].0);
-    }
-
-    println!("{}", prev_distance);
+    println!("{}", fp(x, &numbers));
 }
 
+fn f(t: f64, values: &Vec<(f64,f64)>) -> f64{
+    let mut largest: f64;
+    let mut smallest: f64;
 
-fn get_i32s() -> Vec<i32> {
+    let mut values_copy = values.clone();
+
+    for i in 0..values.len() {
+        values_copy[i].0 += values_copy[i].1 * t;
+    }
+
+    largest =  0.0;
+    smallest = 0.0;
+    
+
+    for (pos, _) in values_copy {
+        largest = if pos > largest {pos} else {largest};
+        smallest = if pos < smallest {pos} else {smallest};
+        println!("{} {}", largest, smallest);
+    }
+
+    println!("{} {}", largest, smallest);
+
+    return diff(&largest, &smallest);
+}
+
+fn fp(t: f64, values: &Vec<(f64, f64)>) -> f64 {
+    let mut min_distance = f64::INFINITY;
+
+    for i in 0..values.len() {
+        for j in (i + 1)..values.len() {
+            let pos_i = values[i].0 + values[i].1 * t;
+            let pos_j = values[j].0 + values[j].1 * t;
+
+            let distance = (pos_i - pos_j).abs();
+            min_distance = min_distance.min(distance);
+        }
+    }
+
+    return min_distance;
+}
+
+fn get_f64s () -> Vec<f64> {
     let mut line = String::new();
     let _ = io::stdin().read_line(&mut line);
 
-    return line.trim().split(' ').map(|c| c.parse().unwrap()).collect::<Vec<i32>>();
+    return line.trim().split(' ').map(|c| c.parse().unwrap()).collect::<Vec<_>>();
 }
 
-fn diff(a: &i32, b: &i32) -> i32 {
-    if (a >= &0 && b >= &0) || (a <= &0 && b <= &0) {
-        return cmp::max(a.abs(), b.abs()) - cmp::min(a.abs(), b.abs());
+fn diff(a: &f64, b: &f64) -> f64 {
+    if (a >= &0.0 && b >= &0.0) || (a <= &0.0 && b <= &0.0) {
+        return f64::max(a.abs(), b.abs()) - f64::min(a.abs(), b.abs());
     } else {
         return a.abs() + b.abs();
     }
+}
+
+fn gss(f: fn (f64, &Vec<(f64,f64)>) -> f64, mut a: f64, mut b: f64, tolerance: f64, gr: f64, values: &Vec<(f64, f64)>) -> f64{
+    // Golden section search. Used pseudocode from https://en.wikipedia.org/wiki/Golden-section_search# 
+
+    let mut c: f64;
+    let mut d: f64;
+    while f64::abs(b - a) > tolerance {
+        c = b - (b - a) / gr;
+        d = a + (b - a) / gr;
+
+        if f(c, &values) < f(d, &values) {
+            b = d;
+        } else {
+            a = c;
+        }
+        println!("a: {}, b: {}", a, b);
+    }
+
+    println!("({} + {}) / 2 = {}", a, b, (a + b) / 2.0);
+    return ((b + a) / 2.0); 
+
 }
