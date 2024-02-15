1eac732624cc32bf43951a6e8d73d84584121c09
Lars Haukland <lars.haukland@gmail.com>
2024-02-15 11:55:43 +0100

    cleaning up code

--- inf237/weeks/week4/cuttingbrownies/solve2.py
+++ inf237/weeks/week4/cuttingbrownies/solve2.py
@@ -1,20 +1,6 @@
 from math import log2
 
 def can_solve(b, d, p):
-    #if b == 1 or d == 1: return "cannot win"
-
-    #if d <= b and d % 2 == 0: return "cannot win"
-
-    #if b <= d and b % 2 == 0: return "cannot win"
-    
-    # if p == "Harry":
-    #     if d > b and d % 2 == 0:
-    #         return "can win"
-    # else:
-    #     if b > d and b % 2 == 0:
-    #         return "can win"
-
-    # return "cannot win"
     lb = int(log2(b))
     ld = int(log2(d))
 
