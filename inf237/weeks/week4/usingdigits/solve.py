bb73e0aba415c9e2c53cb8ac45d96a3050e6928d
Lars Haukland <lars.haukland@gmail.com>
2024-02-20 16:30:39 +0100

    Some bug but idk

--- inf237/weeks/week4/usingdigits/solve.py
+++ inf237/weeks/week4/usingdigits/solve.py
@@ -16,34 +16,44 @@ for y in range(height):
     dp[y] = {}
     for x in range(width):
         dp[y][x] = {}
-        for possiblekey in [key[0:i] for i in range(len(key), -1, -1)]:
+        for possiblekey in [key[i:] for i in range(len(key), -1, -1)]:
+            print(possiblekey)
             if x == width - 1 and y == height - 1:
                 dp[y][x][possiblekey] = grid[y][x]
             else:
-                dp[y][x][possiblekey] = 1000000000
+                dp[y][x][possiblekey] = float('inf')
 
-print(dp)
 
 #print(key)
-for x in range(width - 1, -1, -1):
-    for y in range(height - 1, -1, -1):
-        for possiblekey in [key[0:i] for i in range(len(key), 0, -1)]:
-            hopDigit = int(possiblekey[0])
-            hopX = x + hopDigit
-            hopY = y + hopDigit
-            nextX = x + 1 if x <= width - 2 else x
-            nextY = y + 1 if y <= height - 2 else y
-            if hopX < width and hopY < height:
-                jumpY = min(dp[nextY][x][possiblekey], dp[hopY][x][possiblekey[1:]])
-                jumpX = min(dp[y][nextX][possiblekey], dp[y][hopX][possiblekey[1:]])
-                print(jumpX, jumpY)
-                dp[y][x][possiblekey] = min(dp[y][x][possiblekey], min(jumpX, jumpY) + grid[y][x])
-            else:
-                jumpY = dp[nextY][x][possiblekey]
-                jumpX = dp[y][nextX][possiblekey]
-                dp[y][x][possiblekey] = min(dp[y][x][possiblekey], min(jumpX, jumpY) + grid[y][x])
+for y in range(height - 1, -1, -1):
+    for x in range(width - 1, -1, -1):
+        for possiblekey in [key[i:] for i in range(len(key) - 1, -1, -1)]:
 
-print((dp[0][0]))
-print(dp[height - 1][width - 2])
+            if x == width - 1 and y == height - 1:
+                continue
+
+            hopDigit = int(possiblekey[0])
+
+            moves = [
+                (1,             0, possiblekey),
+                (0,             1, possiblekey),
+                (hopDigit + 1,  0, possiblekey[1:]),
+                (0,  hopDigit + 1, possiblekey[1:])
+            ]
+
+            for (dx, dy, key) in moves:
+                if x + dx <= width - 1  and y + dy <= height - 1:
+                    print("JIPPI")
+                    dp[y][x][possiblekey] = min(dp[y][x][possiblekey], dp[y + dy][x + dx][key] + grid[y][x]) 
+                else:
+                    print(f"These were not in bounds: {x + dx = }, {y + dy = }")
+
+
+for y in range(height - 1, -1, -1):
+    print()
+    for x in range(width - 1, -1, -1):
+        print(dp[y][x])
+print(min(dp[0][0].values()))
+print(dp[height - 1][width - 1])
                 
             
