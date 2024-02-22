34057b7a98b471ef4f27b5d0cdf1cc7582191ce1
Lars Haukland <lars.haukland@gmail.com>
2024-02-22 12:22:47 +0100

    Solved it'

--- inf237/weeks/week4/usingdigits/solve.py
+++ inf237/weeks/week4/usingdigits/solve.py
@@ -17,22 +17,21 @@ for y in range(height):
     for x in range(width):
         dp[y][x] = {}
         for possiblekey in [key[i:] for i in range(len(key), -1, -1)]:
-            print(possiblekey)
             if x == width - 1 and y == height - 1:
                 dp[y][x][possiblekey] = grid[y][x]
             else:
                 dp[y][x][possiblekey] = float('inf')
 
 
-#print(key)
-for y in range(height - 1, -1, -1):
+for possiblekey in [key[i:] for i in range(len(key), -1, -1)]:
     for x in range(width - 1, -1, -1):
-        for possiblekey in [key[i:] for i in range(len(key) - 1, -1, -1)]:
+        for y in range(height - 1, -1, -1):
 
             if x == width - 1 and y == height - 1:
                 continue
 
-            hopDigit = int(possiblekey[0])
+            hopDigit = int(possiblekey[0]) if len(possiblekey) >= 1 else 0
+
 
             moves = [
                 (1,             0, possiblekey),
@@ -41,19 +40,17 @@ for y in range(height - 1, -1, -1):
                 (0,  hopDigit + 1, possiblekey[1:])
             ]
 
-            for (dx, dy, key) in moves:
-                if x + dx <= width - 1  and y + dy <= height - 1:
-                    print("JIPPI")
-                    dp[y][x][possiblekey] = min(dp[y][x][possiblekey], dp[y + dy][x + dx][key] + grid[y][x]) 
-                else:
-                    print(f"These were not in bounds: {x + dx = }, {y + dy = }")
+
+            for (dx, dy, key1) in moves:
+                if (x + dx <= width - 1)  and (y + dy <= height - 1):
+                    dp[y][x][possiblekey] = min(dp[y][x][possiblekey], dp[y + dy][x + dx][key1] + grid[y][x]) 
 
 
-for y in range(height - 1, -1, -1):
-    print()
-    for x in range(width - 1, -1, -1):
-        print(dp[y][x])
-print(min(dp[0][0].values()))
-print(dp[height - 1][width - 1])
+#for y in range(height - 1, -1, -1):
+#    print()
+#    for x in range(width - 1, -1, -1):
+#        print(dp[y][x])
+print(dp[0][0][key])
+#print(dp[height - 1][width - 1])
                 
             
