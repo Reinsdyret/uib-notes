432a7ff98e668fdc6112d92a1ea8f08e07a7e601
Lars Haukland <ves019@uib.no>
2024-03-15 14:25:08 +0100

    Finally solved

--- inf237/weeks/week7/countingtriangles/solution.py
+++ inf237/weeks/week7/countingtriangles/solution.py
@@ -7,21 +7,51 @@ class Point:
         return f"({self.x}, {self.y})"
 
 def orientation(p1, q1, p2):
-    return ((q1.y - p1.y) *
+    val = ((q1.y - p1.y) *
            (p2.x - q1.x) -
            (q1.x - p1.x) *
            (p2.y - q1.y))
 
+    if val > 0:
+        return 1 # Clockwise rotation
+    elif val < 0:
+        return -1 # Counterclockwise rotation
+    else:
+        return 0 # Collinear orientation
+
+def on_segment(p, q, r):
+    # Check if point q lies on line segment 'pr'
+    if (q[0] <= max(p[0], r[0]) and q[0] >= min(p[0], r[0]) and
+       q[1] <= max(p[1], r[1]) and q[1] >= min(p[1], r[1])):
+        return True
+    return False
+
+
 def intersection_test(l1, l2):
-    p1, q1 = l1
+    p1, q1 = l1 # p1 is start of l1 and q1 is end of l1
     p2, q2 = l2
-    o1 = orientation(p1, q1, p2)
+    o1 = orientation(p1, q1, p2) # Check where start of l2 lies in terms of l1 (right, left or colinear)
     o2 = orientation(p1, q1, q2)
     o3 = orientation(p2, q2, p1)
     o4 = orientation(p2, q2, q1)
 
-    return o1 != o2 and o3 != o4 and o1 != 0 and o2 != 0 and o3 != 0 and o4 != 0
+    if o1 != o2 and o3 != o4: # General case, are the start and end of each line of seperate sides of the other line
+        return True
 
+    # Special cases
+    if o1 == 0 and on_segment(p1, p2, q1): # If the start of l2 is colinear with l1 and it actually is on the line it intersects
+        return True
+
+    if o2 == 0 and on_segment(p1, q2, q1):
+        return True
+
+    if o3 == 0 and on_segment(p2, p1, q2):
+        return True
+
+    if o4 == 0 and on_segment(p2, q1, q2):
+        return True
+
+    return False
 
 n = int(input())
 while n != 0:
@@ -42,7 +72,7 @@ while n != 0:
                     p1, q1 = l1
                     p2, q2 = l2
                     p3, q3 = l3
-                    print(f"TRIANGLE with {(str(p1), str(q1))}, {(str(p2), str(q2))}, {(str(p3), str(q3))}")
+                    #print(f"TRIANGLE with {(str(p1), str(q1))}, {(str(p2), str(q2))}, {(str(p3), str(q3))}")
                     count += 1
     print(count)
     n = int(input())
