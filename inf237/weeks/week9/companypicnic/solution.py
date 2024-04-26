bee920a7fb23f5fd8c4eae1b296032b86771f083
Lars Haukland <lars.haukland@gmail.com>
2024-04-26 14:44:44 +0200

    Very spread out commit

--- inf237/weeks/week9/companypicnic/solution.py
+++ inf237/weeks/week9/companypicnic/solution.py
@@ -1,30 +1,49 @@
-import networkx as nx
-G = nx.Graph()
+from decimal import *
+getcontext().prec = 10
+
+def solve(v, skip, graph, memo, speed):
+    if len(graph[v]) == 0: return (0, 0) # If bottom of the tree, return 0 teams and 0 speed under
+    if (v, skip) in memo: return memo[(v, skip)]
+
+    best_teams = 0
+    best_speed = 0 
+    for neighbour in graph[v]:
+        skip_teams, skip_speed = solve(neighbour, True, graph, memo, speed)
+        best_teams += skip_teams
+        best_speed += skip_speed
+
+    best = (best_teams, best_speed)
+    
+    if skip:
+        for neighbour in graph[v]:
+            skip_teams, skip_speed = solve(neighbour, True, graph, memo, speed)
+            taken_teams, taken_speed = solve(neighbour, False, graph, memo, speed)
+
+            teams = best_teams - skip_teams + taken_teams + 1
+            speeds = best_speed - skip_speed + min(speed[v], speed[neighbour]) + taken_speed
+
+            best = max(best, (teams, speeds))
+
+    memo[(v, skip)] = best 
+    return best
+
 
 n = int(input())
-edges = []
-nodes = {}
+adjlist = {}
+memory = {}
+speed = {}
+
 for _ in range(n):
     a, w, b = input().split()
     w = float(w)
-    nodes[a] = w
-    G.add_node(a)
-    if b != "CEO":
-        edges.append((a,b))
+    if b not in adjlist:
+        adjlist[b] = [a]
+    else:
+        adjlist[b].append(a)
 
-for (a,b) in edges:
-    weight = min(nodes[a], nodes[b])
-    G.add_edge(a,b,weight=weight)
-    
+    if a not in adjlist:
+        adjlist[a] = []
+    speed[a] = w
 
-#G.add_weighted_edges_from(edges)
-max_match = nx.max_weight_matching(G)
-
-avg_match = 0
-for (a,b) in max_match:
-    weight = min(nodes[a], nodes[b])
-    avg_match += weight
-
-avg_match /= len(max_match)
-
-print(f"{len(max_match)} {avg_match}")
+teams, sums = solve('CEO', False, adjlist, memory, speed)
+print(f"{teams} {sums / teams}")
