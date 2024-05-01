cf446b9d1b3553942c26489ce0f2373bf7456a8d
Lars Haukland <lars.haukland@gmail.com>
2024-05-01 14:07:06 +0200

    Done stuff

--- inf115/virtual_env/test/test.py
+++
@@ -1,9 +0,0 @@
-
-from shiny.express import input, render, ui
-
-ui.input_slider("n", "N", 0, 100, 20)
-
-
-@render.text
-def txt():
-    return f"n*2 is {input.n() * 2}"
---
+++ inf237/weeks/week10/tomography/solution.py
@@ -0,0 +1,8 @@
+n, m = map(int, input().split())
+
+row_sums = list(map(int, input().split()))
+col_sums = list(map(int, input().split()))
+
+
+
+
