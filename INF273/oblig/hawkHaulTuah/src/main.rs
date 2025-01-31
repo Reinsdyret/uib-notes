59c4b6882093259d56e8b1c1ec7ac5eb67d2c09f
Lars Haukland <lars.haukland@proton.me>
2025-02-01 00:09:07 +0100

    Some cleaning up

--- INF273/oblig/hawkHaulTuah/src/main.rs
+++ INF273/oblig/hawkHaulTuah/src/main.rs
@@ -1,8 +1,13 @@
 
 use file_reader::parse_data::*;  // Import read_file function
+use checker::checker::*;
 
 fn main() {
-    let filename = "src/data/Call_18_Vehicle_5.txt";  // Update the path as needed
+    let solution: Vec<u32> = vec![0,0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7];
+    
+    let filename = "src/data/Call_7_Vehicle_3.txt";  // Update the path as needed
     
     let instance = read_file(filename);
+
+    println!("{:?}", check_feasibility_and_get_cost(&instance, solution));
 }
